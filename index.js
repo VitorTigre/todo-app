2b86f26b0faf4342e3b8b6c7fdde3e2476a2d69f
VitorTigre <vitorrtigre@gmail.com>
2023-11-29 12:56:36 -0300

    filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -33,6 +33,26 @@ app.post('/completar', (requisicao, resposta) => {
         })
 })
 
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    console.log(id)
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, erro => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar', (requisicao, resposta) =>{
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -69,7 +89,13 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-        resposta.render('home', {tarefas})
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefaAtivas = tarefasAtivas.legth
+
+        resposta.render('home', {tarefas, quatidadeTarefasSAtivas})
     })
 })
 
